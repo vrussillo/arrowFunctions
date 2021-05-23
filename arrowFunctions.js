601c5502a44cdcb06faf5befa40154cf0a82dbc9
vrussillo <80196694+vrussillo@users.noreply.github.com>
2021-05-23 14:16:50 -0700

    arrow_function_exercise

---
+++ arrowFunctions.js
@@ -0,0 +1,29 @@
+[2, 5, 6, 177, 153, 99, 33].reduce(function (max, currNum) {
+  return Math.max(max, currNum);
+});
+
+//arrow function
+[2, 5, 6, 177, 153, 99, 33].reduce((max, currNum) => {
+  return Math.max(max, currNum);
+});
+
+// function double(arr) {
+//     return arr.map(function(val) {
+//       return val * 2;
+//     });
+//   }
+
+const double = (arr) => arr.map((val) => val * 2);
+
+// function squareAndFindEvens(numbers) {
+//   var squares = numbers.map(function (num) {
+//     return num ** 2;
+//   });
+//   var evens = squares.filter(function (square) {
+//     return square % 2 === 0;
+//   });
+//   return evens;
+// }
+
+const squareAndFindEvens = (numbers) =>
+  numbers.map((num) => num ** 2).filter((square) => square % 2 === 0);
